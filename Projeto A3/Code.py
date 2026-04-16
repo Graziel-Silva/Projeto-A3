cf4bad7533b8eb18f353bcc4063daaea5250ed32
Wesley Oliveira <wesleyob27@gmail.com>
2026-04-15 23:10:04 -0300

    Update Code.py

--- Projeto A3/Code.py
+++ Projeto A3/Code.py
@@ -1,12 +1,24 @@
 import math
 
-def ler_numero(msg):
+  def ler_numero(msg):
     return float(input(msg).replace(",", "."))
 
-for x in range(4):
+  for x in range(4):
      print("\n CALCULADORA GEOMÉTRICA")
      print("1 - Quadrado")
      print("2 - Circulo")
      print("3 - Triangulo")
 
     op = input("Escolha uma opçao")
+
+   match op:
+
+      case 1:
+           lado = ler_numero("Lado:")
+           area = lado * lado
+           perimetro = 4 * lado
+           volume = lado ** 3
+
+           print("Área:", area)
+           print("Perímetro:", perimetro)
+           print("Volume:", volume)
