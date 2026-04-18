5ee29440234755946189eaf32c6e7045e67582f0
Wesley Oliveira <wesleyob27@gmail.com>
2026-04-18 20:55:55 -0300

    Update Code.py

--- Projeto A3/Code.py
+++ Projeto A3/Code.py
@@ -3,7 +3,7 @@ import math
 def ler_numero(msg):
     return float(input(msg).replace(",", "."))
 
-  for x in range(6):
+  for x in range(7):
      print("\n CALCULADORA GEOMÉTRICA")
      print("1 - Quadrado")
      print("2 - Circulo")
@@ -36,8 +36,25 @@ def ler_numero(msg):
 
            print("Área:", (base * altura) / 2)
            print("Perímetro:", l1 + l2 + l3)
-           rint("Volume: Não possui")
+           print("Volume: Não possui")
     
       case 4 :
+            base = ler_numero("Área da base: ")
+            altura = ler_numero("Altura: ")
+
+            volume = (1/3) * base * altura
+
+            print("Volume da pirâmide:", volume)
+
+      case "5": 
+            lado = ler_numero("Lado: ")
+            perimetro = 5 * lado
+
+            print("Perímetro:", perimetro)
+            print("Área: (fórmula mais complexa)")
+            print("Volume: Não possui")
+
+      case _:
+            print("Opção inválida!")
 
         
